e66206e27e299aa7236502de43b63f9a633fc521
badawiiz <badawiiz@mail.uc.edu>
2021-09-08 16:49:04 -0400

    updated sysinfo.ps1 code

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,1 +1,3 @@
-﻿Write-Host("This machine's IP address is {0}" -f $IP)
+﻿function getIP{(Get-NetIPAddress).IPv4Address | Select-String "192*"};
+$IP = getIP
+Write-Host("This machine's IP address is {0}" -f $IP)
