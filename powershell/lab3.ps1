9ca815f948c128b1cc7a556cbdb3947d0a24b513
badawiiz <badawiiz@mail.uc.edu>
2021-09-08 18:29:36 -0400

    lab3.ps1 file added

---
+++ powershell/lab3.ps1
@@ -0,0 +1,18 @@
+﻿function getIP{(Get-NetIPAddress).IPv4Address | Select-String "192*"};
+$IP = getIP
+$USER = $env:USERNAME
+$HOSTNAME = hostname
+$PSVERSION = $HOST.Version.Major
+$DAYOFWEEK = (Get-Date).DayOfWeek
+$MONTH = (Get-Date).Month
+$DAY = (Get-Date).Day
+$YEAR = (Get-Date).Year
+
+$BODY = "This machines IP address is $IP. User is $USER. Hostname is $HOSTNAME. PowerShell Version $PSVERSION. Today's date is $DAYOFWEEK, $MONTH/$DAY/$YEAR."
+
+Write-Host($BODY)
+
+$BODY | Out-File C:\lab3.txt
+
+
+Send-MailMessage -To "anthony.bothe@uc.edu" -From "emadbadawiziad@gmail.com" -Subject "IT3038C Windows sysinfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSsl -Credential (Get-Credential)
