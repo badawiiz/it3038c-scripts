1c5ee1ad404112727cc205e82a569360b0f6771d
badawiiz <badawiiz@mail.uc.edu>
2021-09-30 13:28:53 -0400

    Last project commit

--- project1/project1.py
+++ project1/project1.py
@@ -8,6 +8,9 @@ user_url = input ('Enter the url endpoint of an api: ')
 # Ask user to enter the local path where the CSV file will be saved at
 user_local_path = input ('Enter the local path you would like the csv file to be saved at: ')
 
+# Ask user to enter the name of the file that is going to be created
+file_name = input ('Enter the name for the new file that is going to be created: ')
+
 # urllib gets the json from the url and stores it in the response variable
 response = urllib.request.urlopen(user_url)
 
@@ -16,7 +19,7 @@ data = json.loads(response.read())
 
 
 #Open a local file and write as a csv file
-with open(f"{user_local_path}/data.csv", "w") as csv_file:
+with open(f"{user_local_path}/{file_name}.csv", "w") as csv_file:
     writer = csv.writer(csv_file)
     for key, value in data.items():
         writer.writerow([key, value])
